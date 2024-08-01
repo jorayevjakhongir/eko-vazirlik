16185818d4c74b5dad598bce1d490b2ad46b982a
Sarvar <57652551+sarvar8373@users.noreply.github.com>
2024-08-01 15:44:43 +0500

    add some file

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 5000;
-
+app.use(bodyParser.json());
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "eco_deparment",
+  password: "",
+  database: "",
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
