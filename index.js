bec735c7234027e03b3120866362b2ae3acd6352
Sarvar <sarvarerkinov21@gmail.com>
2024-08-04 15:45:53 +0500

    new changing add

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
 const express = require("express");
 const session = require("express-session");
 const cookie = require("cookie-parser");
-const path = require("path");
+const dotenv = require("dotenv");
 const ejs = require("ejs");
-const multer = require("multer");
-const async = require("async");
-const nodemailer = require("nodemailer");
-const crypto = require("crypto");
-const expressValidator = require("express-validator");
-const sweetalert = require("sweetalert2");
 const bodyParser = require("body-parser");
 const http = require("http");
 const db = require("./models/db_controller.js");
-
+const authRotes = require("./routes/authRoutes.js");
 const app = express();
-const signup = require("./controllers/signup.js");
-const home = require("./controllers/home.js");
-const login = require("./controllers/login.js");
-const verify = require("./controllers/verify.js");
-const reset = require("./controllers/reset_controller.js");
-var landing = require("./controllers/landing.js");
+
 app.set("view engine", "ejs");
 
-const server = http.createServer(app);
+dotenv.config();
 // Middleware for parsing JSON and URL-encoded bodies
 app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,9 +23,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-app.use("/login", login);
-app.use("/home", home);
-app.use("/", landing);
-app.use("/signup", signup);
-app.use("/verify", verify);
-app.use("/reset", reset);
+
+app.get("/", (req, res) => res.render("home"));
+app.use(authRotes);
--- models/db_controller.js
+++ models/db_controller.js
@@ -1,33 +1,33 @@
-const mysql = require("mysql");
-const express = require("express");
-const router = express.Router();
-const bodyParser = require("body-parser");
+// const mysql = require("mysql");
+// const express = require("express");
+// const router = express.Router();
+// const bodyParser = require("body-parser");
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "eco_deparment",
-});
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "eco_department",
+// });
 
-con.connect(function (err) {
-  if (err) {
-    throw err;
-  } else {
-    console.log("you are connected to database");
-  }
-});
+// con.connect(function (err) {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log("you are connected to database");
+//   }
+// });
 
-module.exports.signup = function (username, email, password, status, callback) {
-  var query =
-    "INSERT INTO `users`(`username`,`email`,`password`,`email_status`) VALUES ('" +
-    username +
-    "','" +
-    email +
-    "','" +
-    password +
-    "','" +
-    status +
-    "')";
-  con.query(query, callback);
-};
+// module.exports.signup = function (username, email, password, status, callback) {
+//   var query =
+//     "INSERT INTO `users`(`username`,`email`,`password`,`email_status`) VALUES ('" +
+//     username +
+//     "','" +
+//     email +
+//     "','" +
+//     password +
+//     "','" +
+//     status +
+//     "')";
+//   con.query(query, callback);
+// };
